be7a7e855df301066b9c43e385713962159b167c
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-01 21:59:06 +0900

    ヘッダーのログインボタンの挙動を実装

--- src/features/ui/Layout.tsx
+++ src/features/ui/Layout.tsx
@@ -1,4 +1,6 @@
+import { pagesPath } from '@/src/utils/$path'
 import { Box } from '@chakra-ui/react'
+import NextLink from 'next/link'
 import { Button } from './Button'
 import { Container } from './Container'
 
@@ -14,9 +16,13 @@ export const Layout: React.FC<Props> = ({ children }) => {
           <Box as="span" fontSize={'2xl'} color="white" fontWeight="bold">
             Frourio Pfl
           </Box>
-          <Button as="div" variant="primaryOutline" ml="auto">
-            ログイン
-          </Button>
+          <Box ml="auto">
+            <NextLink href={pagesPath.login.$url()}>
+              <Button as="div" variant="primaryOutline">
+                ログイン
+              </Button>
+            </NextLink>
+          </Box>
         </Container>
       </Box>
       <Container as="main">{children}</Container>
