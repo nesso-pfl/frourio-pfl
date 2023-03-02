020d24d3a484ff886c0bacd1c02d539bc6eeea40
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-03 07:23:41 +0900

    認証用ページを仮追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import { NextPage } from 'next'
 import { AuthCheck } from '../features/account'
+import { Layout } from '../features/ui'
 
 const Home: NextPage = () => {
-  return <AuthCheck>home</AuthCheck>
+  return (
+    <AuthCheck>
+      <Layout>home</Layout>
+    </AuthCheck>
+  )
 }
 
 export default Home
