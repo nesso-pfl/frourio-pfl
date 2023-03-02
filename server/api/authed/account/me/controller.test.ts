6faac373e0fed47bb066f8699dc9517a7519d4d5
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-02 19:41:26 +0900

    GET /public/me -> GET /authed/account/me に移動

--- server/api/authed/account/me/controller.test.ts
+++ server/api/authed/account/me/controller.test.ts
@@ -2,7 +2,7 @@ import { mockAccount } from '$/mocks'
 import fastify from 'fastify'
 import controller from './controller'
 
-describe('GET /public/me', () => {
+describe('GET /authed/account/me', () => {
   test('自身のアカウント情報が取得できる', async () => {
     const controller_ = controller(fastify())
 
