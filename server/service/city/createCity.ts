2f06c733d190c912abcba784407fa63c67d6267c
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-23 14:56:04 +0900

    CreateCity を実装

--- server/service/city/createCity.ts
+++ server/service/city/createCity.ts
@@ -1,12 +1,14 @@
 import { depend } from 'velona'
 import { prisma } from '$/lib/prisma'
 import { CreateCity, City } from '$/types'
-import { z } from 'zod'
 
-const create = async ({}: CreateCity) => {
-  return await prisma.city.create({ data: { firebaseUid } })
+const create = async (newCity: CreateCity) => {
+  const { features, ...restData } = newCity
+  return await prisma.city.create({
+    data: { ...restData, features: { create: features } },
+    include: { features: true },
+  })
 }
 export const createCity = depend({ create }, async ({ create }, newCity: CreateCity): Promise<City> => {
-
-
+  return await create(newCity)
 })
