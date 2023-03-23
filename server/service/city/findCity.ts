d4b50c7470497b3559022e3343d11d46fbe42cb9
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-23 15:00:50 +0900

    findCity を実装

--- server/service/city/findCity.ts
+++ server/service/city/findCity.ts
@@ -1,15 +1,17 @@
 import { prisma } from '$/lib/prisma'
+import { City } from '$/types'
 import { depend } from 'velona'
 
 const find = async (id: number) => {
   const city = await prisma.city.findUnique({
     where: { id },
+    include: { features: true },
   })
 
   if (!city) return undefined
 
   return city
 }
-export const findCity = depend({ find }, async ({ find }, id: number) => {
+export const findCity = depend({ find }, async ({ find }, id: number): Promise<City | undefined> => {
   return await find(id)
 })
