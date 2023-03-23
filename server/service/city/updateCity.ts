11c534d370c70ed41bfb0691d377c8b87b9530ac
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-23 16:03:41 +0900

    updateCity を実装

--- server/service/city/updateCity.ts
+++ server/service/city/updateCity.ts
@@ -1,16 +1,21 @@
 import { depend } from 'velona'
-import { UpdateCity } from '$/types'
+import { City, UpdateCity } from '$/types'
 import { prisma } from '$/lib/prisma'
 
 const update = async (id: number, params: UpdateCity) => {
+  const { features, ...restParams } = params
   const city = await prisma.city.update({
     where: { id },
-    data: params,
+    data: {
+      ...restParams,
+      features: { connectOrCreate: features.map((feature) => ({ create: feature, where: { name: feature.name } })) },
+    },
+    include: { features: true },
   })
 
   return city
 }
 
-export const updateCity = depend({ update }, async ({ update }, id: number, params: UpdateCity) => {
+export const updateCity = depend({ update }, async ({ update }, id: number, params: UpdateCity): Promise<City> => {
   return update(id, params)
 })
