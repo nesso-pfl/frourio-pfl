4819b5df86d866d9ddf35156f222c43aa2792514
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-23 14:49:36 +0900

    CityFeature の型定義を追加

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,4 +1,5 @@
 export type { City, CreateCity, CityQuery, UpdateCity } from './city'
+export type { CityFeature, CreateCityFeature, CityFeatureQuery, UpdateCityFeature } from './cityFeature'
 export { CreateUserError, createUserErrorCodes } from './user'
 export type { User, CreateUser } from './user'
 export { createAccountErrorCodes, CreateAccountError } from './account'
