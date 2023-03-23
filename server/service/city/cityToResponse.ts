5cffd09123e11e8dd52b7a18175d91e8f0703b53
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-23 14:58:11 +0900

    cityToResponse を実装

--- server/service/city/cityToResponse.ts
+++ server/service/city/cityToResponse.ts
@@ -3,6 +3,9 @@ import { Response, City } from '$/types'
 export const cityToResponse = (city: City): Response<City> => {
   return {
     ...city,
+    startedAt: city.startedAt.toISOString(),
+    // TODO: cityFeatureToResponse を使う
+    features: [],
     createdAt: city.createdAt.toISOString(),
     updatedAt: city.updatedAt.toISOString(),
   }
