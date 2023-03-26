2c54839ce0fd79dfe73c2b2026e92fbd03451f95
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-26 13:25:36 +0900

    eslint-plugin-react-hooks を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'prettier',
