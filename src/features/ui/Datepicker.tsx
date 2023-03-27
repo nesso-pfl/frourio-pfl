f7accbe12f6447b1e2d2ff81621819d344ff1d09
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-28 08:33:11 +0900

    Datepicker のデフォルト props を設定

--- src/features/ui/Datepicker.tsx
+++ src/features/ui/Datepicker.tsx
@@ -2,11 +2,20 @@ import ReactDatePicker, { ReactDatePickerProps, registerLocale } from 'react-dat
 import ja from 'date-fns/locale/ja'
 
 import 'react-datepicker/dist/react-datepicker.css'
+import { Input } from './Input'
 
 registerLocale('ja', ja)
 
 type Props = Omit<ReactDatePickerProps, 'locale'>
 
 export const Datepicker: React.FC<Props> = (props) => {
-  return <ReactDatePicker locale={ja} {...props} />
+  return (
+    <ReactDatePicker
+      locale={ja}
+      placeholderText="年/月/日"
+      dateFormat="yyyy/MM/dd"
+      customInput={<Input />}
+      {...props}
+    />
+  )
 }
