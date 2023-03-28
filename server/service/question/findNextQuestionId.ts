1350c63eec312ddd5d571cb5e363f8aec26a56ee
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-28 22:22:36 +0900

    findNextQuestion を修正

--- server/service/question/findNextQuestionId.ts
+++ server/service/question/findNextQuestionId.ts
@@ -1,8 +1,8 @@
-import { mockQuestions } from '$/mocks'
 import { Answer } from '$/types'
 
 export const findNextQuestionId = (answers: Answer[]): number | undefined => {
   const lastAnswer = answers.at(-1)
   // TODO: 今までの回答内容から次の質問を決定する
-  return mockQuestions().find((question) => question.id === lastAnswer?.questionId ?? 1 + 1)?.id
+  const nextQuestionId = (lastAnswer?.questionId ?? 1) + 1
+  return nextQuestionId
 }
