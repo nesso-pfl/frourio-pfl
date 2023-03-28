8aa2cd082a5c1cb587fcebea2e508c6de6a7443d
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-28 21:17:00 +0900

    CheckboxQuestionCard を実装

---
+++ src/features/question/CheckboxQuestionCard.tsx
@@ -0,0 +1,55 @@
+import { Answer, Question } from '$/types'
+import { Box, Flex, VStack } from '@chakra-ui/react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button, Card, CheckboxGroup, Container, Form } from '../ui'
+
+const formSchema = z.object({
+  values: z.coerce.number().array(),
+})
+type Form = z.infer<typeof formSchema>
+
+type Props = {
+  question: Question<'checkbox'>
+  onSubmit: (answer: Answer<'checkbox'>) => Promise<unknown>
+}
+
+export const CheckboxQuestionCard: React.FC<Props> = ({ question, onSubmit }) => {
+  const { control, handleSubmit } = useForm<Form>({ resolver: zodResolver(formSchema) })
+
+  const onSubmit_ = async (formValues: Form) => {
+    await onSubmit(formValues.values)
+  }
+
+  return (
+    <Container maxW={['100%', '640px']}>
+      <Card.Container>
+        <Card.Body display="flex" flexDirection="column" alignItems="center" py={16}>
+          <Box as="p" mb={16}>
+            {question.content}
+          </Box>
+          <Form.Container onSubmit={handleSubmit(onSubmit_)}>
+            <Controller
+              control={control}
+              name="values"
+              render={({ field: { value, onChange } }) => (
+                <CheckboxGroup
+                  value={value}
+                  onChange={onChange}
+                  options={question.answers.map((answer) => ({ label: answer.label, value: answer.value.toString() }))}
+                />
+              )}
+            />
+            <VStack></VStack>
+            <Flex w="80%" gap={8}>
+              <Button type="submit" size="lg" w="50%">
+                次へ
+              </Button>
+            </Flex>
+          </Form.Container>
+        </Card.Body>
+      </Card.Container>
+    </Container>
+  )
+}
---
+++ src/features/question/index.ts
@@ -0,0 +1,2 @@
+export { CheckboxQuestionCard } from './CheckboxQuestionCard'
+export { YesNoQuestionCard } from './YesNoQuestionCard'
