c492437828dc12b9e8127fb0addf60bfb80f5d64
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-03-29 21:22:14 +0900

    Prettier を実行

--- server/api/authed/question/_questionId@number/validators.ts
+++ server/api/authed/question/_questionId@number/validators.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 import { defineValidators } from './$relay'
 
 export default defineValidators(() => ({
-  params: z.object({ questionId: z.number() })
+  params: z.object({ questionId: z.number() }),
 }))
